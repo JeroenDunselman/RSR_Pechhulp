48ad26d3fa430710e9bac99cbdf39ff5f2320fd3
Jeroen Dunselman <jeroduns@gmail.com>
2015-07-06 07:24:59 +0200

    Initial Commit

---
+++ RSR Pechhulp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RSR Pechhulp
+//
+//  Created by Jeroen Dunselman on 06/07/15.
+//  Copyright (c) 2015 Jeroen Dunselman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ RSR PechhulpTests/RSR_PechhulpTests.m
@@ -0,0 +1,40 @@
+//
+//  RSR_PechhulpTests.m
+//  RSR PechhulpTests
+//
+//  Created by Jeroen Dunselman on 06/07/15.
+//  Copyright (c) 2015 Jeroen Dunselman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface RSR_PechhulpTests : XCTestCase
+
+@end
+
+@implementation RSR_PechhulpTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
